dc6e2f51a953caf8905c3ca581edbfd18ac3c6c4
DiSkills <anti.python.fastapi@gmail.com>
2022-12-28 16:37:17 +0300

    Added Depth-first search tests

---
+++ tests/graph/test_dfs.py
@@ -0,0 +1,25 @@
+from src.graph.dfs import get_quantity_connectivity_components
+
+
+def test_get_quantity_connectivity_components() -> None:
+    # A - B - C - D
+    graph = {
+        'A': {'B'},
+        'B': {'A', 'C'},
+        'C': {'B', 'D'},
+        'D': {'C'},
+    }
+    quantity = get_quantity_connectivity_components(graph=graph)
+    assert quantity == 1
+    
+    # A - B
+    # \   /
+    #   C   .D
+    graph = {
+        'A': {'B', 'C'},
+        'B': {'A', 'C'},
+        'C': {'A', 'B'},
+        'D': {},
+    }
+    quantity = get_quantity_connectivity_components(graph=graph)
+    assert quantity == 2
